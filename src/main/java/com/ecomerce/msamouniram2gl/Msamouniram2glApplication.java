9a405ceb9f856a130286a1213d3d8dd00d38f63e
Mouny Queen <latufasoilihiboina@gmail.com>
2020-03-29 00:17:25 +0000

    ajout des dependances

---
+++ src/main/java/com/ecomerce/msamouniram2gl/Msamouniram2glApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.msamouniram2gl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msamouniram2glApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msamouniram2glApplication.class, args);
+	}
+
+}
