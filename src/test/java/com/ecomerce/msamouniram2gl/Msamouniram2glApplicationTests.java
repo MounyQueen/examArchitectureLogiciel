734ada2f500bd2fe31b5cc6263c29b46a17e1081
Mouny Queen <latufasoilihiboina@gmail.com>
2020-03-29 00:53:14 +0000

    les librairaires pour les dependances des persistances

---
+++ src/test/java/com/ecomerce/msamouniram2gl/Msamouniram2glApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.msamouniram2gl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msamouniram2glApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
